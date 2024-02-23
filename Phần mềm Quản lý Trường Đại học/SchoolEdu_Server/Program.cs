d3a236b595e8648a1f92808a3f894f8fc2f8a158
thanglele <lesythang2@gmail.com>
2024-02-24 01:16:19 +0700

    Update Program.cs

--- Phần mềm Quản lý Trường Đại học/SchoolEdu_Server/Program.cs
+++ Phần mềm Quản lý Trường Đại học/SchoolEdu_Server/Program.cs
@@ -10,6 +10,8 @@ namespace SchoolEdu_Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
