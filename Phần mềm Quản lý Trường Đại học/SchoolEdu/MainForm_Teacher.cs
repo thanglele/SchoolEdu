ccf848c4f5a57b388b708e6d9c2b7e4a97f232ed
thanglele <lesythang2@gmail.com>
2024-02-26 00:23:05 +0700

    Update Form Users

---
+++ Phần mềm Quản lý Trường Đại học/SchoolEdu/MainForm_Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolEdu
+{
+    public partial class MainForm_Teacher : Form
+    {
+        public MainForm_Teacher()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Phần mềm Quản lý Trường Đại học/SchoolEdu/Program.cs
+++ Phần mềm Quản lý Trường Đại học/SchoolEdu/Program.cs
@@ -16,7 +16,7 @@ namespace SchoolEdu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login_Form());
         }
     }
 }
