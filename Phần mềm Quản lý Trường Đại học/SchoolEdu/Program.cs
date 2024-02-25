d69fa30e85d7dc247d9a4041f1fed39bb0882ecc
thanglele <lesythang2@gmail.com>
2024-02-26 00:28:52 +0700

    Update Net, TCP/IP - Client

--- Phần mềm Quản lý Trường Đại học/SchoolEdu/Program.cs
+++ Phần mềm Quản lý Trường Đại học/SchoolEdu/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace SchoolEdu
 {
