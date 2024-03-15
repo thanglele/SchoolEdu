76cd6f762c8a770df8783bb761a8a0e2ab11cdd6
thanglele <lesythang2@gmail.com>
2024-03-15 23:07:56 +0700

    Update database, SQL

--- SChool_Edu.sql
+++ SChool_Edu.sql
@@ -1,53 +1,53 @@
 use SchoolEdu_Database;
 
-create table NhanVien (
-	MaGV nvarchar(30) not null primary key,
-	TenGV nvarchar(30) not null,
+create table GIAOVIEN (
+	MaGV nvarchar not null primary key,
+	TenGV nvarchar not null,
 	NgaySinh date,
-	GioiTinh nvarchar(30) not null,
-	DiaChi nvarchar(30) not null,
-	MatKhau nvarchar(30) not null,
-	VaiTro nvarchar(30) not null;
+	GioiTinh nvarchar not null,
+	DiaChi nvarchar not null,
+	MatKhau nvarchar not null,
+	VaiTro nvarchar not null;
 );
 
 create table MONHOC
 (
-	MaMH nvarchar(30) not null primary key,
-	TenMH nvarchar(30) not null,
+	MaMH nvarchar not null primary key,
+	TenMH nvarchar not null,
 	SoTC int,
 );
 
 create table LOP
 (
-	MaLop nvarchar(30) not null primary key,
-	Khoa nvarchar(30) not null,
-	Nganh nvarchar(30) not null,
-	Nienkhoa nvarchar(30) not null,
-	MaGV nvarchar(30) not null,
+	MaLop nvarchar not null primary key,
+	Khoa nvarchar not null,
+	Nganh nvarchar not null,
+	Nienkhoa nvarchar not null,
+	MaGV nvarchar not null,
 	constraint FK_MaGV foreign key (MaGV) references NhanVien(MaGV),
 );
 
 create table SINHVIEN
 (
-	MaSV nvarchar(30) not null primary key,
-	TenSV nvarchar(30) not null,
+	MaSV nvarchar not null primary key,
+	TenSV nvarchar not null,
 	NgaySinh date,
-	GioiTinh nvarchar(30),
-	DiaChi nvarchar(30),
-	MatKhau nvarchar(30),
-	MaLop nvarchar(30) not null,
+	GioiTinh nvarchar,
+	DiaChi nvarchar,
+	MatKhau nvarchar,
+	MaLop nvarchar not null,
 	constraint FK_MaLop foreign key (MaLop) references LOP(MaLop),
 );
 
 create table DiemSV
 (
-	MaSV nvarchar(30) not null,
-	MaMH nvarchar(30) not null,
+	MaSV nvarchar not null,
+	MaMH nvarchar not null,
 	primary key (MaSV, MaMH),
 	LanThi int,
 	DiemQT float,
 	DiemThi float,
-	CaThi nvarchar(30),
+	CaThi nvarchar,
 	NgayThi date,
 	constraint FK_MaSV foreign key (MaSV) references SINHVIEN(MaSV),
 	constraint FK_MaMH foreign key (MaMH) references MONHOC(MaMH),
@@ -55,20 +55,20 @@ create table DiemSV
 
 create table GVMH
 (
-	MaGV nvarchar(30) not null,
-	MaMH nvarchar(30) not null,
+	MaGV nvarchar not null,
+	MaMH nvarchar not null,
 	primary key(MaGV, MaMH),
 	Ngaybd date,
-	Phonghoc nvarchar(30),
-	CaHoc nvarchar(30),
+	Phonghoc nvarchar,
+	CaHoc nvarchar,
 	constraint FK_MaGV2 foreign key (MaGV) references NhanVien(MaGV),
 	constraint FK_MaMH2 foreign key (MaMH) references MONHOC(MaMH),
 );
 
 create table SVMH
 (
-	MaSV nvarchar(30) not null,
-	MaMH nvarchar(30) not null,
+	MaSV nvarchar not null,
+	MaMH nvarchar not null,
 	primary key(MaSV, MaMH),
 	Ngaybatdau date,
 	Ngayketthuc date,
